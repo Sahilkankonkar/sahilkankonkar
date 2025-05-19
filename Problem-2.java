ccbd46878d191a26e60523ba5e470613870fc057
Sahilkankonkar <sahilnkankonkar@gmail.com>
2025-05-19 20:47:03 +0530

    Create Problem-2.java
    Series of odd numbers

---
+++ Problem-2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number (a): ");
+        int a = sc.nextInt();
+
+        int num = 1; // First odd number
+
+        for (int i = 0; i < a; i++) {
+            System.out.print(num);
+            if (i < a - 1) {
+                System.out.print(", ");
+            }
+            num += 2; // Move to next odd number
+        }
+
+        sc.close();
+    }
+}
